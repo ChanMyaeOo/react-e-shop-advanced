ee225464ce912607e8a28bfa7d652b427d21022c
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-09 09:40:29 +0630

    saved authenticated user to firestore and stored firestore data to our app state

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,28 @@ const config = {
   appId: "1:170081251199:web:eeb9e54c65edf60117744a",
 };
 
+export const createUserProfileDocument = async (authUser, additionalData) => {
+  if (!authUser) return;
+  const userRef = firestore.doc(`/users/${authUser.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = authUser;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message());
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
