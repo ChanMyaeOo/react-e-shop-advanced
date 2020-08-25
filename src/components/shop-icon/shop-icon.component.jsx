c6d9b3e901a51416f3772680447625bdc66222f4
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-25 11:26:35 +0630

    added reselect library and implemented memorization for cart-selectors

--- src/components/shop-icon/shop-icon.component.jsx
+++ src/components/shop-icon/shop-icon.component.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartDropdown } from "../../redux/cart/cart-actions";
+import { selectCartItemsCount } from "../../redux/cart/cart-selectors";
 import "./shop-icon.styles.scss";
 
-const ShopIcon = ({ toggleCartDropdown }) => (
+const ShopIcon = ({ toggleCartDropdown, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartDropdown}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown()),
 });
-export default connect(null, mapDispatchToProps)(ShopIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(ShopIcon);
