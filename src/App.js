06cb65579ee20abd20671ca9579a327a04cf3b3b
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-26 11:05:00 +0630

    added checkout item component and implemented addItem and removeItem

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Checkout from "./components/checkout/checkout.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user-actions";
@@ -39,6 +40,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/checkout" component={Checkout} />
           <Route
             path="/signin"
             exact
