0ba77c83531f656632d8087ab5e082b6604434b6
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-30 17:59:49 +0630

    added nested route for shop page and created selectCollection function in the shop.selectors.js

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop-selectors";
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return <div>Collection Page</div>;
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
+  console.log(match);
   return (
     <div className="shop">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
